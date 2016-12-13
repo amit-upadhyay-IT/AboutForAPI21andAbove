5084877158a528e033ad0531513974c6c1c25df1
amit-upadhyay-IT <youarefreak1@gmail.com>
2016-12-14 01:29:30 +0530

    apk size reduced

--- app/src/main/java/com/amitupadhyay/aboutexample/MainActivity.java
+++ app/src/main/java/com/amitupadhyay/aboutexample/MainActivity.java
@@ -1,10 +1,12 @@
 package com.amitupadhyay.aboutexample;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.amitupadhyay.aboutexample.ui.AboutActivity;
 
@@ -20,7 +22,13 @@ public class MainActivity extends AppCompatActivity {
         clickMe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, AboutActivity.class));
+                if (Build.VERSION.SDK_INT < 21) {
+                    Toast.makeText(MainActivity.this, "About can't be displayed because api is less than 21", Toast.LENGTH_SHORT).show();
+                }
+                else if (Build.VERSION.SDK_INT >= 21) {
+                    Toast.makeText(MainActivity.this, "Lollipop or greater device", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(MainActivity.this, AboutActivity.class));
+                }
             }
         });
     }
